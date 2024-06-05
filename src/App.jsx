618deabe9fafa06fd1480a4d936c25a675b54da7
Nugroho Wicakso <nugrhwicaks@gmail.com>
2024-06-05 15:00:03 +0700

    proyek todolist 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,72 @@
+import React, { useState, createContext } from 'react'
+import Todos from "./components/Todos";
+import TodoForm from "./components/TodoForm";
+export const TodoContext = createContext()
+
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Finish Progate React Course",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Have lunch with Guru Domba",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Study React with Ninja Ken",
+      completed: false,
+    },
+  ]);
+
+  console.log(todos);
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+  const deleteTodo = () => { 
+  };
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
+  }
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+    <TodoContext.Provider value={{ toggleCompleted }}>
+      <div style={styles.container}>
+        <h1 style={styles.title}>My Todo List</h1>
+        <TodoForm addTodo={addTodo} />
+        <Todos todos={todos} />
+      </div>
+    </TodoContext.Provider>
+  )
 }
 
-export default App;
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
+}
+
+export default App
